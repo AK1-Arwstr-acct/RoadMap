2032ed993839f8538275d1a8c3719e50ab8a3cd7
mohidkhan87 <mohidkhan87@gmail.com>
2025-05-09 19:38:23 +0500

    check hotjar

--- app.vue
+++ app.vue
@@ -9,7 +9,6 @@
   </div>
 </template>
 <script setup lang="ts">
-
 const { locale } = useI18n();
 const { t } = useI18n();
 
@@ -30,11 +29,9 @@ useHead(
         name: "description",
         content: t("meta.homepage.description"),
       },
-      { property: 'og:image', content: '/images/ogimage.png' },
+      { property: "og:image", content: "/images/ogimage.png" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/png', href: '/favicon.png' }
-    ]
+    link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
   }))
 );
 
@@ -45,7 +42,7 @@ const appStore = useAppStore();
 const hotjarConfig = () => {
   const runtimeConfig = useRuntimeConfig();
   if (import.meta.client) {
-    if(runtimeConfig.public.appMode == "https://app.arrowster.com") {
+    if (runtimeConfig.public.appMode == "https://app.arrowster.com") {
       (function (h, o, t, j, a, r) {
         h.hj =
           h.hj ||
@@ -59,7 +56,9 @@ const hotjarConfig = () => {
         r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
         a.appendChild(r);
       })(window, document, "https://static.hotjar.com/c/hotjar-", ".js?sv=");
-    } else if(runtimeConfig.public.appMode == "https://roadmap.arrowster.com") {
+    } else if (
+      runtimeConfig.public.appMode == "https://roadmap.arrowster.com"
+    ) {
       (function (h, o, t, j, a, r) {
         h.hj =
           h.hj ||
@@ -77,8 +76,30 @@ const hotjarConfig = () => {
   }
 };
 
+function getHotjarSessionId() {
+  const siteId = 6372129;
+  // const hotjarUserCookie = useCookie(`_hjSessionUser_6372129`); // Exact match of the cookie name
+
+  // console.log(hotjarUserCookie.value); // Check if it returns the expected cookie value
+
+  // console.log("check:", `_hjSessionUser_${siteId}`);
+  // // const hotjarId =
+  // //   hotjarCookie && hotjarCookie.split("=")[0] === `_hjSession_${siteId}`
+  // //     ? hotjarCookie.split("=")[1]
+  // //     : null;
+
+  // // console.log("Hotjar Session ID:", hotjarId);
+  const cookies = document.cookie.split(";").map((cookie) => cookie.trim());
+  console.log("Cookies:", cookies);
+  const hotjarCookie = cookies.find((cookie) => cookie.startsWith(`_hjSession_${siteId}`));
+  console.log("hotjarCookie:", hotjarCookie);
+}
+
 onMounted(async () => {
   await appStore.getUserData();
-  hotjarConfig();
+  await hotjarConfig();
+  setTimeout(() => {
+    getHotjarSessionId();
+  }, 3000);
 });
 </script>
