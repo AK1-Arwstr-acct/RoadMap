25748df2e34e85769840d8bfc2d9963cbc909eb4
mohidkhan87 <mohidkhan87@gmail.com>
2025-05-09 19:31:57 +0500

    hotjar header added

--- composables/useApi.ts
+++ composables/useApi.ts
@@ -9,12 +9,18 @@ export const useApi = () => {
     },
   });
 
-  
   api.interceptors.request.use(config => {
-    const token = useCookie('token')
+    const token = useCookie('token');
+    const hotjarId = window.hj ? window.hj.q?.find(([event]) => event === 'hjid')?.[1] : null;
+
     if (token.value) {
-      config.headers["Authorization"] = `Bearer ${token.value}`
+      config.headers["Authorization"] = `Bearer ${token.value}`;
     }
+
+    if (hotjarId) {
+      config.headers["hotjarId"] = hotjarId;
+    }
+
     return config;
   }, error => {
     return Promise.reject(error);
@@ -24,12 +30,12 @@ export const useApi = () => {
     response => response,
     error => {
       if (error.response && error.response.status === 401) {
-        useCookie('token').value = null
-        navigateTo('/login')
+        useCookie('token').value = null;
+        navigateTo('/login');
       }
       return Promise.reject(error);
     }
   );
 
   return { api };
-}
+};
